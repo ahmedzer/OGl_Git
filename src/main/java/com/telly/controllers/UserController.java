f7b1e41b38aa4716ff5b411fa87b3901a13d35a4
Lounis <Lounisakli15@gmail.com>
2023-01-08 17:57:00 +0100

    Fonctionnalité deconnexion

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -7,4 +7,10 @@ public class UserController {
     public String showLogin() {
         return "login";
     }
+
+    @RequestMapping("/loggedout")
+    public String showLogout() {
+        return "loggedout";
+    }
+
 }
